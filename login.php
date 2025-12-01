3d2e919f25331552d1fcbc514de94f9dcfe8394e
imamprayudi <imamprayudi@gmail.com>
2025-12-01 14:34:06 +0700

    login

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<script>
+let urllogin = 'http://136.198.117.118/api/jkeiitinv1dev/login.php';
+
+async function login(userid)
+{
+  let iduser = userid;
+  try 
+  {
+    const response = await fetch(urllogin, 
+    {
+      method: 'POST',
+      credentials: "include",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        userid: iduser
+      })
+    });
+    const reply = await response.text();
+    console.log(reply);
+  
+  }catch (error)
+  {
+    console.error(error);
+  }
+}
+login('31530');
+</script>
+</body>
+</html>
